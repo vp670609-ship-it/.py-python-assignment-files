c646bf78735551fc2a10d6bef4b9760d62fdb3e0
vp670609-ship-it <vp670609@gmail.com>
2026-09-19 08:36:58 +0530

    Add files via upload

---
+++ varibles.py
@@ -0,0 +1,18 @@
+name = "Venkatesh"
+age = 20
+marks = 85
+price = 99.5
+city = "Chennai"
+print(city)
+score = 95
+print(score)
+x = 10
+print(x)
+message = "Welcome"
+print(message)
+a = 10
+b = 20
+print(a + b)
+quantity = 5
+price = 10
+print(quantity * price)
